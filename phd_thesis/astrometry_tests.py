5cc64a2b32b6ba64e3a78379529c45d1a5993aab
Lachlan <lachlan.marnoch@students.mq.edu.au>
2022-09-29 18:46:05 +1000

    Reliability and speed.

--- phd_thesis/astrometry_tests.py
+++ phd_thesis/astrometry_tests.py
@@ -75,18 +75,20 @@ def main(
         return do
 
     def experiment(
-            epoch: field.FORS2ImagingEpoch,
+            epoch_name_this: str,
             exp_name: str,
             stage_names: list,
             message: str,
             frames_name: str,
             coadded_name: str,
             stage_kwargs: dict = {},
+            epoch_this=None,
             switch_coadded: dict = None,
     ):
         """
         The function below is just for convenience, in order to avoid copy/pasting code; for the passed Epoch object,
-        :param epoch: the Epoch on which the test is being run.
+        :param epoch_name_this: the name of the epoch on which the test is being run.
+        :param epoch_this: the Epoch on which the test is being run.
         :param exp_name: the experiment name with which to label dictionary entries, paths etc.
         :param stage_names: names of reduction stages to perform for this test.
         :param message: message to print before the test.
@@ -97,87 +99,96 @@ def main(
         :param switch_coadded: a dictionary of CoaddedImage objects to replace the Epoch's coadded dict.
         :return:
         """
+
         # Set up dictionaries to catch status and results for this experiment.
-        if exp_name not in status[epoch_name]:
-            status[epoch_name][exp_name] = {
+        if exp_name not in status[epoch_name_this]:
+            status[epoch_name_this][exp_name] = {
                 "processing": None,
                 "analysis": None
             }
-        if exp_name not in files[epoch_name]:
-            files[epoch_name][exp_name] = {
+        if exp_name not in files[epoch_name_this]:
+            files[epoch_name_this][exp_name] = {
                 "coadded_from": None,
                 "diagnostics_from": None
             }
-        # Set up epoch directory
-        test_dir_epoch = os.path.join(test_dir, epoch_name)
-        u.mkdir_check(test_dir_epoch)
-        # Override normal output path with our test directory
-        test_dir_epoch_exp = os.path.join(test_dir_epoch, exp_name)
-        u.mkdir_check(test_dir_epoch_exp)
-        epoch.data_path = test_dir_epoch_exp
-        # Build the path where the new Epoch output file will be located.
-        new_output_file = os.path.join(
-            epoch.data_path,
-            f"{epoch.name}_outputs.yaml"
-        )
-
-        if not os.path.isfile(new_output_file):
-            # Read in output file from normal reduction
-            epoch.output_file = old_output_file
-            epoch.load_output_file()
-            # Switch path to output file
-            epoch.output_file = new_output_file
-            # Write copy of old output file to new directory
-            epoch.update_output_file()
-
-        # Set the Epoch output file to the new path, so that values derived from these images go here instead of to the
-        # primary epoch file.
-        epoch.output_file = new_output_file
-        epoch.load_output_file()
-
-        if switch_coadded is not None:
-            epoch.coadded_astrometry = {}
-            for fil in switch_coadded:
-                epoch.add_coadded_astrometry_image(
-                    img=switch_coadded[fil],
-                    key=fil
-                )
-        ############
-        # Processing
-        ############
-        # Do this test?
         do = do_this(exp_name=exp_name)
         if do:
+            print("Initialising epoch...")
+            if epoch_this is None:
+                epoch_this = field.FORS2ImagingEpoch.from_params(
+                    name=epoch_name_this,
+                    instrument="vlt-fors2",
+                    field=fld
+                )
+            # Set up epoch directory
+            test_dir_epoch = os.path.join(test_dir, epoch_name_this)
+            u.mkdir_check(test_dir_epoch)
+            # Override normal output path with our test directory
+            test_dir_epoch_exp = os.path.join(test_dir_epoch, exp_name)
+            u.mkdir_check(test_dir_epoch_exp)
+            epoch_this.data_path = test_dir_epoch_exp
+            # Build the path where the new Epoch output file will be located.
+            new_output_file = os.path.join(
+                epoch_this.data_path,
+                f"{epoch_this.name}_outputs.yaml"
+            )
+
+            if not os.path.isfile(new_output_file):
+                # Read in output file from normal reduction
+                epoch_this.output_file = old_output_file
+                epoch_this.load_output_file()
+                # Switch path to output file
+                epoch_this.output_file = new_output_file
+                # Write copy of old output file to new directory
+                epoch_this.update_output_file()
+
+            # Set the Epoch output file to the new path, so that values derived from these images go here instead of to the
+            # primary epoch file.
+            epoch_this.output_file = new_output_file
+            epoch_this.load_output_file()
+
+            if switch_coadded is not None:
+                epoch_this.coadded_astrometry = {}
+                for fil in switch_coadded:
+                    epoch_this.add_coadded_astrometry_image(
+                        img=switch_coadded[fil],
+                        key=fil
+                    )
+            ############
+            # Processing
+            ############
+            # Do this test?
+
             try:
                 print(f"Doing {message} (processing)")
-                epoch.do = stage_names
+                epoch_this.do = stage_names
                 # Set up the special parameters for this experiment
-                for stage_name in epoch.do:
-                    if stage_name not in epoch.param_file:
-                        epoch.param_file[stage_name] = {}
+                for stage_name in epoch_this.do:
+                    if stage_name not in epoch_this.param_file:
+                        epoch_this.param_file[stage_name] = {}
                     # Force the stages to be performed as required, overwriting the input param file
-                    epoch.do_kwargs[stage_name] = True
-                if "coadd" not in epoch.param_file:
-                    epoch.param_file["coadd"] = {}
-                epoch.param_file["coadd"]["frames"] = frames_name
-                if "source_extraction" not in epoch.param_file:
-                    epoch.param_file["source_extraction"] = {}
-                epoch.param_file.update(stage_kwargs)
-                epoch.param_file["source_extraction"]["image_type"] = coadded_name
+                    epoch_this.do_kwargs[stage_name] = True
+                if "coadd" not in epoch_this.param_file:
+                    epoch_this.param_file["coadd"] = {}
+                epoch_this.param_file["coadd"]["frames"] = frames_name
+                if "source_extraction" not in epoch_this.param_file:
+                    epoch_this.param_file["source_extraction"] = {}
+                epoch_this.param_file.update(stage_kwargs)
+                epoch_this.param_file["source_extraction"]["image_type"] = coadded_name
                 # So that we're on an even footing for all tests, set the offset tolerance for astrometry diagnostics to
                 # 2 arcseconds
-                epoch.param_file["source_extraction"]["offset_tolerance"] = 2 * units.arcsec
+                epoch_this.param_file["source_extraction"]["offset_tolerance"] = 2 * units.arcsec
                 # Run the epoch's pipeline, on the relevant stages and with the correct input parameters, as established
                 # above.
-                epoch.pipeline()
-                epoch.update_output_file()
+                epoch_this.pipeline()
+                epoch_this.update_output_file()
                 # Tell the 'files' file which files offsets were actually measured from
-                files[epoch_name][exp_name]["coadded_from"] = field._output_img_dict_list(
-                    epoch._get_frames(frames_name))
-                files[epoch_name][exp_name]["diagnostics_from"] = field._output_img_dict_single(
-                    epoch._get_images(coadded_name))
+                files[epoch_name_this][exp_name]["coadded_from"] = field._output_img_dict_list(
+                    epoch_this._get_frames(frames_name))
+                files[epoch_name_this][exp_name]["diagnostics_from"] = field._output_img_dict_single(
+                    epoch_this._get_images(coadded_name))
                 # If all of this worked, set status to 'fine'. If it didn't, the exception below will trigger.
-                status[epoch_name][exp_name]["processing"] = "fine"
+                status[epoch_name_this][exp_name]["processing"] = "fine"
             except:
                 # This too-broad except block exists entirely as a convenience so that failed epochs/tests get skipped
                 # over gracefully without having to rerun the script.
@@ -185,9 +196,10 @@ def main(
                 # Catch and save traceback for failure, in case something can be learned from it.
                 for tb in traceback.format_tb(sys.exc_info()[2]):
                     tb_message.append(str(tb))
-                status[epoch_name][exp_name]["processing"] = tb_message
+                status[epoch_name_this][exp_name]["processing"] = tb_message
                 p.save_params(status_path, status)
                 p.save_params(file_path, files)
+            
             p.save_params(status_path, status)
             ##########
             # Analysis
@@ -195,21 +207,21 @@ def main(
             try:
                 # Pull astrometry test results out of output files.
                 print(f"Doing {message} (analysis)")
-                results_epoch_ast = epoch.astrometry_stats
-                results_epoch_psf = epoch.psf_stats
+                results_epoch_ast = epoch_this.astrometry_stats
+                results_epoch_psf = epoch_this.psf_stats
                 results_epoch = {
                     "astrometry": results_epoch_ast,
                     "psf": results_epoch_psf
                 }
 
-                if epoch_name not in results:
-                    results[epoch_name] = {}
-                results[epoch_name][exp_name] = results_epoch
-                epoch.output_file = os.path.join(
-                    epoch.data_path,
-                    f"{epoch.name}_outputs.yaml")
-                epoch.update_output_file()
-                status[epoch_name][exp_name]["analysis"] = "fine"
+                if epoch_name_this not in results:
+                    results[epoch_name_this] = {}
+                results[epoch_name_this][exp_name] = results_epoch
+                epoch_this.output_file = os.path.join(
+                    epoch_this.data_path,
+                    f"{epoch_this.name}_outputs.yaml")
+                epoch_this.update_output_file()
+                status[epoch_name_this][exp_name]["analysis"] = "fine"
             except:
                 # This too-broad except block exists entirely as a convenience so that failed epochs/tests get skipped
                 # over gracefully without having to rerun the script.
@@ -217,10 +229,12 @@ def main(
                 tb_message = []
                 for tb in traceback.format_tb(sys.exc_info()[2]):
                     tb_message.append(str(tb))
-                status[epoch_name][exp_name]["analysis"] = tb_message
-            epoch.update_output_file()
+                status[epoch_name_this][exp_name]["analysis"] = tb_message
+            epoch_this.update_output_file()
             p.save_params(status_path, status)
             p.save_params(results_path, results)
+
+        del epoch_this
 
     if exclude is not None:
         exclude = exclude.split(",")
@@ -279,7 +293,7 @@ def main(
                     f"{epoch.name}_outputs.yaml"
                 )
                 experiment(
-                    epoch=epoch,
+                    epoch_name_this=epoch_name,
                     exp_name="1-no_correction",
                     stage_names=[
                         "coadd",
@@ -287,21 +301,16 @@ def main(
                     ],
                     message="1. Coaddition without correction",
                     frames_name="normalised",
-                    coadded_name="coadded"
+                    coadded_name="coadded",
+                    epoch_this=epoch,
                 )
                 del epoch
 
                 ######################################
                 # 2. Offline Gaia on coadded image
                 ######################################
-                print("Initialising epoch...")
-                epoch = field.FORS2ImagingEpoch.from_params(
-                    name=epoch_name,
-                    instrument="vlt-fors2",
-                    field=fld
-                )
                 experiment(
-                    epoch=epoch,
+                    epoch_name_this=epoch_name,
                     exp_name="2-gaia_coadded",
                     stage_names=[
                         "coadd",
@@ -312,21 +321,13 @@ def main(
                     frames_name="normalised",
                     coadded_name="coadded_astrometry",
                     stage_kwargs={"correct_astrometry_coadded": {"correct_to_epoch": True}}
-
                 )
-                del epoch
 
                 ######################################
                 # 3. Offline Gaia on individual, no epoch correction
                 ######################################
-                print("Initialising epoch...")
-                epoch = field.FORS2ImagingEpoch.from_params(
-                    name=epoch_name,
-                    instrument="vlt-fors2",
-                    field=fld
-                )
                 experiment(
-                    epoch=epoch,
+                    epoch_name_this=epoch_name,
                     exp_name="3-gaia_individual_no_epoch_correction",
                     stage_names=[
                         "correct_astrometry_frames",
@@ -338,19 +339,12 @@ def main(
                     coadded_name="coadded",
                     stage_kwargs={"correct_astrometry_frames": {"correct_to_epoch": False}}
                 )
-                del epoch
 
                 ######################################
                 # 4. Offline Gaia on individual, with epoch correction
                 ######################################
-                print("Initialising epoch...")
-                epoch = field.FORS2ImagingEpoch.from_params(
-                    name=epoch_name,
-                    instrument="vlt-fors2",
-                    field=fld
-                )
                 experiment(
-                    epoch=epoch,
+                    epoch_name_this=epoch_name,
                     exp_name="4-gaia_individual_epoch_correction",
                     stage_names=[
                         "correct_astrometry_frames",
@@ -362,7 +356,6 @@ def main(
                     coadded_name="coadded",
                     stage_kwargs={"correct_astrometry_frames": {"correct_to_epoch": True}}
                 )
-                del epoch
 
                 ######################################
                 # 5. Online Astrometry.net on coadded images
@@ -406,7 +399,8 @@ def main(
                     # If the final file is missing, then nova.astrometry.net hasn't worked.
                     if os.path.isfile(output_file):
                         experiment(
-                            epoch=epoch,
+                            epoch_this=epoch,
+                            epoch_name_this=epoch_name,
                             exp_name=exp_name_this,
                             stage_names=[
                                 "source_extraction",
