0f52a2a885099eb62193cc033683ece9b2607852
Lachlan <lachlan.marnoch@students.mq.edu.au>
2022-10-20 12:08:40 +1100

    Minor doc change

--- Ryder+2022FRB20220610A/host_imaging.py
+++ Ryder+2022FRB20220610A/host_imaging.py
@@ -6,7 +6,7 @@ of Ryder et al 2022 (arXiv: https://arxiv.org/abs/2210.04680)
 
 Prerequisites:
 - `craft-optical-followup`; tested on `ryder+2022` branch (latest commit):
-  https://github.com/Lachimax/craft-optical-followup/tree/ryder+2022
+    https://github.com/Lachimax/craft-optical-followup/tree/ryder+2022
 - `astropy`; tested with version `5.0.4`
 - `matplotlib`; tested with version `3.5.2`
 """
